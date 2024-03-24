593af4b6f998968e86f07817b537234655f74c3e
Marat Kashaev <marat.kashaev@pearson.com>
2024-03-25 01:42:11 +0300

    Refactoring

---
+++ frontend/src/constants/apiPaths.ts
@@ -0,0 +1,15 @@
+const API_PATHS = {
+  // product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  // order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  // import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  // bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  // cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+
+  product: "http://localhost:4173",
+  order: "http://localhost:4173",
+  import: "http://localhost:4173",
+  bff: "http://localhost:4173",
+  cart: "http://localhost:4173",
+};
+
+export default API_PATHS;
